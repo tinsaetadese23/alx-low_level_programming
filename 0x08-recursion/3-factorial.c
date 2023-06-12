0d1e9ba3ccd768952196d1954a82ceabbdddb91d
Tinsae Tadese <tinsaetadese23@gmail.com>
2023-06-12 04:57:48 -0700

    Calculating factorial of a give num

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* factorial - returns the factorial of a given number
+* @n: the number to calculate the factorial of
+*
+* Return: the factorial of n as an integer, or -1 if n is negative
+*/
+int factorial(int num)
+{
+if (num < 0)
+return (-1);
+if (num == 0)
+return (1);
+return (num * factorial(num - 1));
+}
