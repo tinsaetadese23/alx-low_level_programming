382c46d1bf35937d544574c64503dfc7f74c8a6c
Tinsae Tadese <tinsaetadese23@gmail.com>
2023-06-12 06:50:27 -0700

    sqrt function using recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@
 * @n: the number to find the square root of
 * @i: the possible square root
 *
-* Return: the square root of n as an integer, or -1 if n has no natural square root
+* Return: the square root of n as an integer, or -1 if n has no natural sqrt fn
 */
 int _helper_function(int n, int i)
 {
@@ -20,7 +20,7 @@ return (_helper_function(n, i + 1));
 * _sqrt_recursion - returns the natural square root of a number
 * @n: the number to find the square root of
 *
-* Return: the square root of n as an integer, or -1 if n has no natural square root
+* Return: the square root of n as an integer, or -1 if n has no natural sqrt fn
 */
 int _sqrt_recursion(int n)
 {
