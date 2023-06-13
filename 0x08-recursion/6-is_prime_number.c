fa700537564ae86e38344b802cc12735f4050c47
Tinsae Tadese <tinsaetadese23@gmail.com>
2023-06-12 23:36:18 -0700

    prime number checker

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+/**
+* is_prime_number - checks if a number is prime
+* @n: the number to check
+* Return: 1 if n is prime, 0 otherwise
+*/
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+if (n == 2 || n == 3)
+return (1);
+if (n % 2 == 0 || n % 3 == 0)
+return (0);
+int i = 5;
+
+while (i * i <= n)
+{
+if (n % i == 0 || n % (i + 2) == 0)
+return (0);
+i += 6;
+}
+return (1);
+}
