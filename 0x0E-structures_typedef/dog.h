be6b5b9a69939094a39c055499a3f42c79ad0655
Tinsae Tadese <tinsaetadese23@gmail.com>
2023-06-20 05:39:39 -0700

    define a struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
 /**
-* @brief A structure to represent a dog
-* 
+* struct dog - a structure to represent a dog
+* @name: the name of the dog
+* @age: the age of the dog in years
+* @owner: the name of the owner of the dog
+*
 * This structure contains three elements: name, age and owner of the dog.
 * The name and owner are pointers to char arrays, and the age is a float value.
 */
 struct dog {
-char *name; /**< The name of the dog */
-float age; /**< The age of the dog in years */
-char *owner; /**< The name of the owner of the dog */
+char *name;
+float age;
+char *owner;
 };
